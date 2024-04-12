ac5c705a6e7ed83b1e99740fba75797ccda0719d
takak2166 <28010438+takak2166@users.noreply.github.com>
2024-04-13 08:56:25 +0900

    add genRandomStringFromRegex

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,13 @@
 /*
 Copyright © 2024 takak
-
 */
 package cmd
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"regexp/syntax"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -17,16 +18,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "randrex",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Usage: randrex [OPTION]... Regexp",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: runRandrex,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -50,6 +45,8 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP("pattern", "p", "", "")
+	rootCmd.Flags().IntP("number", "n", 1, "Number of print")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -73,5 +70,46 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func runRandrex(cmd *cobra.Command, args []string) {
+	pattern, _ := cmd.Flags().GetString("pattern")
+
+	re, err := syntax.Parse(pattern, syntax.Perl)
+	if err != nil {
+		return
+	}
+
+	generatedStr := genRandomStringFromRegex(re)
+	fmt.Println(generatedStr)
+}
+
+func genRandomStringFromRegex(re *syntax.Regexp) string {
+	var result []rune
+	generateFromNode(re, &result)
+	return string(result)
+}
+
+func generateFromNode(node *syntax.Regexp, result *[]rune) {
+	switch node.Op {
+	case syntax.OpCharClass:
+		*result = append(*result, rune(node.Rune[rand.Intn(len(node.Rune))]))
+	case syntax.OpRepeat:
+		for i := 0; i < node.Min; i++ {
+			generateFromNode(node.Sub[0], result)
+		}
+	case syntax.OpConcat:
+		for _, sub := range node.Sub {
+			generateFromNode(sub, result)
+		}
+	case syntax.OpAlternate:
+		generateFromNode(node.Sub[rand.Intn(len(node.Sub))], result)
+	case syntax.OpCapture:
+		for _, sub := range node.Sub {
+			generateFromNode(sub, result)
+		}
+	default:
+		// 他のノード型は無視する
 	}
 }
---
+++ cmd/root_test.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"regexp"
+	"regexp/syntax"
+	"testing"
+)
+
+func TestGenRandomStringFromRegex(t *testing.T) {
+	patterns := []string{
+		`^[a-z]{3}\d{2}[A-Z]{2}$`,
+		`^hoge_.*`,
+	}
+
+	for _, pattern := range patterns {
+		re, err := syntax.Parse(pattern, syntax.Perl)
+		if err != nil {
+			t.Errorf("failed to parse pattern: %v", err)
+			return
+		}
+		generatedStr := genRandomStringFromRegex(re)
+
+		matched, err := regexp.MatchString(pattern, generatedStr)
+		if err != nil {
+			t.Errorf("error matching string: %v", err)
+			return
+		}
+
+		if !matched {
+			t.Errorf("generated string %q does not match expected pattern %q", generatedStr, pattern)
+		}
+	}
+}
